ea4793e0fa6a545b638e8779df3973c5c1ec5343
Mrbysco <Mrbysco@users.noreply.github.com>
2026-03-21 00:12:42 +0100

    Port to 1.21.1

--- src/main/java/com/mrbysco/elytracancel/ElytraCancelMod.java
+++ src/main/java/com/mrbysco/elytracancel/ElytraCancelMod.java
@@ -1,7 +1,7 @@
 package com.mrbysco.elytracancel;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(ElytraCancelMod.MOD_ID)
