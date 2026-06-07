7f8332e9a1ebf42cffbb4ef4b3da476d1c0ca8fb
Mrbysco <Mrbysco@users.noreply.github.com>
2026-06-07 18:38:20 +0200

    Port to 1.21.1

---
+++ forge/src/main/java/com/mrbysco/elytracancel/ElytraCancelNeoForge.java
@@ -0,0 +1,12 @@
+package com.mrbysco.elytracancel;
+
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class ElytraCancelNeoForge {
+
+	public ElytraCancelNeoForge() {
+
+	}
+}
--- forge/src/main/java/com/mrbysco/elytracancel/platform/NeoForgePlatformHelper.java
+++ forge/src/main/java/com/mrbysco/elytracancel/platform/NeoForgePlatformHelper.java
@@ -2,6 +2,6 @@ package com.mrbysco.elytracancel.platform;
 
 import com.mrbysco.elytracancel.platform.services.IPlatformHelper;
 
-public class ForgePlatformHelper implements IPlatformHelper {
+public class NeoForgePlatformHelper implements IPlatformHelper {
 
 }
